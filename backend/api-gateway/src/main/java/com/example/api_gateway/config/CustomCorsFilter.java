219c7ef666567be3850586e3ad7003b401f6a4c9
hoainam298 <nguyenhoainam29.08.01@gmail.com>
2025-06-19 17:02:16 +0700

    update create account when register

--- backend/api-gateway/src/main/java/com/example/api_gateway/config/CustomCorsFilter.java
+++ backend/api-gateway/src/main/java/com/example/api_gateway/config/CustomCorsFilter.java
@@ -17,10 +17,10 @@ public class CustomCorsFilter implements WebFilter {
         var request = exchange.getRequest();
         var response = exchange.getResponse();
 
-        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
         response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.getHeaders().add("Access-Control-Max-Age", "3600");
-        response.getHeaders().add("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+        response.getHeaders().add("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token, x-api-key");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod().name())) {
             response.setStatusCode(org.springframework.http.HttpStatus.OK);
--- backend/api-gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
+++ backend/api-gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                                 "/api/customers/register/initiate",
                                 "/api/customers/register/kyc-and-otp",
                                 "/api/customers/register/confirm",
+                                "/account/api/v1/create-initial-payment-account",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html"
