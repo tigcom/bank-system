2a19048bd934a9194c53da8d25343eda6ac273e9
daile269 <daile2692003@gmail.com>
2025-07-01 17:25:20 +0700

    Update logging and configs

--- backend/transaction-service/src/main/java/com/example/transaction_service/controller/TransactionController.java
+++ backend/transaction-service/src/main/java/com/example/transaction_service/controller/TransactionController.java
@@ -16,7 +16,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
